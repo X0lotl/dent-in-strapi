1e70c9aa94b19c383aff4f7c67fac31209fc8dbf
X0lotl <danil200319771@gmail.com>
2023-03-08 21:47:27 +0200

    imrove: add work categorty to works

---
+++ src/api/work-category/controllers/work-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::work-category.work-category');
---
+++ src/api/work-category/routes/work-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::work-category.work-category');
